fee96d07859ceabc2e12086459392084d4e1d89c
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-09-27 11:09:22 -0300

    Initial Commit

---
+++ ComponentsSwiftUI/ComponentsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsSwiftUIApp.swift
+//  ComponentsSwiftUI
+//
+//  Created by Christian Manzaraz on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
